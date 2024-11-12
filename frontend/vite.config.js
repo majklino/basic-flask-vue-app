a531649497927c35c3adce5443d27ff0a1bdcc17
Červinka <mcervinka17@gmail.com>
2024-11-12 14:26:55 +0100

    changed default port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server:{
+    port: 6952
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
